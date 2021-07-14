146ae975bb7ecad0a02b426e8b0015757dab81d6
PrasannaThuraisingam647 <55007017+PrasannaThuraisingam647@users.noreply.github.com>
2021-07-14 16:12:42 -0400

    Created Reaction Tester

---
+++ src/components/Results.vue
@@ -0,0 +1,31 @@
+<template>
+  <h3 class="rank">{{rank}}</h3>
+  <p>Reaction Time - {{score}} ms</p>
+</template>
+
+<script>
+  export default {
+    props: ['score'],
+    data() {
+      return {
+        rank: null
+      }
+    },
+    mounted() {
+      if (this.score < 250) {
+        this.rank = 'Ninja Fingers'
+      } else if (this.score < 400) {
+        this.rank = 'Rapid Reflexes'
+      } else {
+        this.rank = 'Snail Pace...'
+      }
+    }
+  }
+</script>
+
+<style>
+  .rank{
+    color: #0faf87;
+    font-weight: bold;
+  }
+</style>
